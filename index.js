d3e8f94f58f22d9c155a4ab05fd2141877f14e52
Emmanuel <emmopk11@outlook.com>
2020-03-31 22:22:01 +0200

    Added process env port variable

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ app.get('/info', (req, res) => {
   res.send(info)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`)
 })
