e38529928bb0e25f9fc1ce5a83ad6b8eb18ae264
Emmanuel <emmopk11@outlook.com>
2020-03-31 22:07:14 +0200

    Committed backend phonebook code to a new repo

---
+++ build/precache-manifest.5b901da91928bf4a3257d23afc6fdc8d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3316f845f407b73efaf0de1d13d02f06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c776e70892d2e35ba3a6",
+    "url": "/static/css/main.0addf385.chunk.css"
+  },
+  {
+    "revision": "1c0e92a3a4c27181168f",
+    "url": "/static/js/2.4b594162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b594162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c776e70892d2e35ba3a6",
+    "url": "/static/js/main.e26d24c4.chunk.js"
+  },
+  {
+    "revision": "9d2b4f7262278411ffe7",
+    "url": "/static/js/runtime-main.4464e187.js"
+  }
+]);
---
+++ index.js
@@ -0,0 +1,119 @@
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+
+let persons = [
+  {
+    name: "Arto Hellas",
+    number: "123-908-6785",
+    id: 1
+  },
+  {
+    name: "Dan Abramov",
+    number: "12-43-234345",
+    id: 3
+  },
+  {
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+    id: 4
+  },
+  {
+    name: "Ada Lovelace",
+    number: "901-800-8776",
+    id: 5
+  }
+]
+
+const generateId = () => (
+  Math.round(Math.random() * 500)
+)
+
+morgan.token('body', function getBody(req) {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
+
+app.get('/api/persons', (req, res) => {
+  res.status(200).json(persons)
+})
+
+app.post('/api/persons', (req, res) => {
+  const { name, number } = req.body
+
+  if (!name) {
+    return res.status(400).json({
+      error: 'Name is missing'
+    })
+  }
+
+  if (!number) {
+    return res.status(400).json({
+      error: 'number is missing'
+    })
+  }
+
+  const personExist = persons.find(
+    person => person.name.toLowerCase() === name.toLowerCase()
+  )
+
+  if (personExist) {
+    return res.status(400).json({
+      error: 'Name must be unique'
+    })
+  }
+
+  const newPerson = {
+    name,
+    number,
+    id: generateId()
+  }
+
+  persons = persons.concat(newPerson)
+  res.status(201).json(newPerson)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if(!person) {
+    return res.status(400).json({
+      error: `person with id ${id} doesn't exist`
+    })
+  } else {
+    res.status(200).json(person)
+  }
+  
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  persons = persons.filter(person => person.id !== id)
+  
+  res.status(204).end()
+})
+
+app.get('/info', (req, res) => {
+  const info = (
+    `<div>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+    </div>`
+  )
+  
+  res.send(info)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+})
