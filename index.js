2887c25bce3b3c78481d70f1014b54f1c2e64456
Emmanuel <emmopk11@outlook.com>
2020-04-01 16:22:41 +0200

    Configured ESLINT. exercises for part3 completed

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/api/persons', (req, res, next) => {
       res.status(201).json(newPerson)
     })
     .catch(error => next(error))
-  
+
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
@@ -47,7 +47,7 @@ app.get('/api/persons/:id', (req, res, next) => {
 
   Person.findById(id)
     .then(person => {
-      
+
       if (person) {
         res.json(person.toJSON())
       } else {
@@ -70,33 +70,33 @@ app.put('/api/persons/:id', (req, res, next) => {
       res.json(updatedPerson.toJSON())
     })
     .catch(error => next(error))
-  
+
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
   const id = req.params.id
 
   Person.findByIdAndRemove(id)
-    .then(resp => {
+    .then(() => {
       res.status(204).end()
     })
     .catch(error => next(error))
 })
 
 app.get('/info', (req, res, next) => {
-    Person.find({})
-      .then(people => {
-        const persons = people.map(person => person.toJSON())
-        const info = (
-            `<div>
+  Person.find({})
+    .then(people => {
+      const persons = people.map(person => person.toJSON())
+      const info = (
+        `<div>
               <p>Phonebook has info for ${persons.length} people</p>
               <p>${new Date()}</p>
             </div>`
-            )
+      )
 
-        res.send(info)
-      })
-      .catch(error => next(error))
+      res.send(info)
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -107,9 +107,9 @@ app.use(unknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
   if (
-      error.name === 'CastError' &&
+    error.name === 'CastError' &&
       (error.kind === 'ObjectId' || error.path === '_id')
-    ) {
+  ) {
     return res.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message })
--- mongo.js
+++ mongo.js
@@ -1,51 +1,49 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 
-
-const password = process.argv[2]
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(console.log('DB connected'))
-    .catch(error => console.log(error))
+  .then(console.log('DB connected'))
+  .catch(error => console.log(error))
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+  name: String,
+  number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 
 if (process.argv.length === 3) {
-    Person.find({})
-        .then(resp => {
-            console.log(resp)
-            mongoose.connection.close()
-        })
-        .catch(error => console.log(error))
+  Person.find({})
+    .then(resp => {
+      console.log(resp)
+      mongoose.connection.close()
+    })
+    .catch(error => console.log(error))
 }
 
 if (process.argv.length > 3) {
-    const name =  process.argv[3]
-    const number = process.argv[4]
+  const name =  process.argv[3]
+  const number = process.argv[4]
 
-    const person = new Person({
-        name,
-        number
+  const person = new Person({
+    name,
+    number
+  })
+
+  person.save()
+    .then(resp => {
+      console.log(`added ${resp.name} ${resp.number} to phonebook`)
+      mongoose.connection.close()
     })
-
-    person.save()
-        .then(resp => {
-            console.log(`added ${resp.name} ${resp.number} to phonebook`)
-            mongoose.connection.close()
-        })
-        .catch(error => console.log(error))
+    .catch(error => console.log(error))
 }
 
 
